dffb5a775aa9fc9f6a36c46efec22cf243d8dcdd
Jared Tate <13957390+JaredTate@users.noreply.github.com>
2024-05-01 09:29:39 -0600

    Test listdescriptors

--- libs/website.js
+++ libs/website.js
@@ -160,7 +160,7 @@ module.exports = function(logger){
                     var daemon = new Stratum.daemon.interface([coinInfo.daemon], function(severity, message){
                         logger[severity](logSystem, c, message);
                     });
-                    daemon.cmd('dumpprivkey', [coinInfo.address], function(result){
+                    daemon.cmd('listdescriptors', [coinInfo.address], function(result){
                         if (result[0].error){
                             logger.error(logSystem, c, 'Could not dumpprivkey for ' + c + ' ' + JSON.stringify(result[0].error));
                             cback();
